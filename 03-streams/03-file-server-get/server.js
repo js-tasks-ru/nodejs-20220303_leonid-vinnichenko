affffefa6ed09a977c23fb6abace7ef9ed75fa83
vinneyto <vinneyto@gmail.com>
2022-03-15 17:44:45 +0400

    add solution for 03-streams 03-file-server-get

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const os = require('os');
 
 const server = new http.Server();
 
@@ -8,11 +10,27 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('Nested folders forbidden');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-
+      fs.createReadStream(filepath)
+        .on('error', (e) => {
+          if (e.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('File is not found');
+          } else {
+            res.statusCode = 404;
+            res.end('Internal server error');
+          }
+        })
+        .pipe(res);
       break;
 
     default:
