64f585e4f3e1e61c5fe4972937a2b578b06880f5
vinneyto <vinneyto@gmail.com>
2022-03-21 19:49:07 +0400

    add test for calculate method

--- 04-testing-configuration-logging/example/server.js
+++ 04-testing-configuration-logging/example/server.js
@@ -1,9 +1,70 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const Validator = require('../01-unit-tests/Validator');
 
 const server = new http.Server();
 
-server.on('request', (_req, res) => {
-  res.end('hello world');
+server.on('request', async (req, res) => {
+  switch (req.url) {
+    case '/':
+      res.end('hello world');
+      break;
+    case '/calculate':
+      const body = [];
+
+      for await (const chunk of req) {
+        body.push(chunk);
+      }
+
+      try {
+        const result = JSON.parse(Buffer.concat(body).toString('utf-8'));
+        const validator = new Validator({
+          a: {
+            type: 'number',
+            min: Number.NEGATIVE_INFINITY,
+            max: Number.POSITIVE_INFINITY,
+          },
+          b: {
+            type: 'number',
+            min: Number.NEGATIVE_INFINITY,
+            max: Number.POSITIVE_INFINITY,
+          },
+        });
+
+        const errors = validator.validate(result);
+
+        if (errors.length > 0) {
+          res.statusCode = 400;
+          res.end(JSON.stringify(errors));
+          return;
+        }
+
+        const { a, b } = result;
+
+        const c = a + b;
+
+        const stream = fs.createWriteStream(path.join(__dirname, 'result.txt'));
+
+        stream.once('error', () => {
+          res.statusCode = 500;
+          res.end('internal server error');
+        });
+
+        stream.once('finish', () => {
+          res.end('calculated');
+        });
+
+        stream.end(c.toString());
+      } catch (e) {
+        res.statusCode = 500;
+        res.end('internal server error');
+      }
+      break;
+    default:
+      res.statusCode = 404;
+      res.end('not found');
+  }
 });
 
 module.exports = server;
--- 04-testing-configuration-logging/example/test/server.test.js
+++ 04-testing-configuration-logging/example/test/server.test.js
@@ -1,19 +1,50 @@
 const got = require('got');
-const assert = require('assert');
+const expect = require('chai').expect;
 const server = require('../server');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3001;
 
 describe('server test', () => {
   before((done) => {
-    server.listen(3001, done);
+    server.listen(PORT, done);
   });
 
   it('get /', async () => {
-    const { body } = await got('http://localhost:3001');
+    const { body } = await got(`http://localhost:${PORT}`);
 
-    assert.strictEqual(body, 'hello world');
+    expect(body).to.equal('hello world');
+  });
+
+  it('post /calucate', async () => {
+    const { statusCode, body } = await got.post(
+      `http://localhost:${PORT}/calculate`,
+      {
+        json: {
+          a: 5,
+          b: 3,
+        },
+      }
+    );
+
+    expect(statusCode).to.equal(200);
+    expect(body).to.equal('calculated');
+
+    const filepath = path.join(__dirname, '..', 'result.txt');
+
+    const result = fs.readFileSync(filepath, { encoding: 'utf-8' });
+
+    expect(+result).to.equal(5 + 3);
   });
 
   after((done) => {
     server.close(done);
+
+    try {
+      fs.unlinkSync(path.join(__dirname, '..', 'result.txt'));
+    } catch (e) {
+      //
+    }
   });
 });
